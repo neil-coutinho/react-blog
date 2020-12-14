5a494bc2d5e7c18fd1c2838d7b54a5f394a44144
Neil Coutinho <njcoutinho@gmail.com>
2020-12-14 16:35:17 +0530

    Fetching posts using axios get and displaying posts components dynamically

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,15 +4,36 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from "axios";
 
 class Blog extends Component {
+
+    constructor(props) {
+        super(props)
+
+       this.state = {
+           posts: []
+       }
+    }
+
+   async componentDidMount() {
+
+        const {data: posts} = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        this.setState({posts})
+    }
+
+
+
     render () {
+
+
+        const posts = this.state.posts.map(({title}, index) => <Post title={title} key={index}/>);
+
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                   { posts }
                 </section>
                 <section>
                     <FullPost />
